bd5cf2034313ab625613f256041260d0d362ff4f
Gerard Hynes <gkhynes@gmail.com>
2021-05-06 22:14:04 +0100

    Start composing some Dadaism

--- markov_chain_composer/graph.py
+++ markov_chain_composer/graph.py
@@ -5,25 +5,28 @@ class Vertex(object):
         self.value = value
         self.adjacent = {}
         self.neighbors = []
-        self.neighbor_weights = []
+        self.neighbors_weights = []
+
+    def __str__(self):
+        return self.value + " ".join([node.value for node in self.adjacent.keys()])
 
     def add_edge_to(self, vertex, weight=0):
         self.adjacent[vertex] = weight
 
     def increment_edge(self, vertex):
-        self.adjacent[vertex] = self.adjacent.get[vertex, 0] + 1
+        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
 
     def get_adjacent_nodes(self):
-        pass
+        return self.adjacent.keys()
 
     # initializes probability map
     def get_probability_map(self):
         for (vertex, weight) in self.adjacent.items():
             self.neighbors.append(vertex)
-            self.neighbor_weights.append(weight)
+            self.neighbors_weights.append(weight)
 
     def next_word(self):
-        return random.choices(self.neighbors, self.neighbor_weights)[0]
+        return random.choices(self.neighbors, weights = self.neighbors_weights)[0]
 
 
 class Graph(object):
@@ -42,7 +45,7 @@ class Graph(object):
         return self.vertices[value]
 
     def get_next_word(self, current_vertex):
-        self.vertices[current_vertex.value].next_word()
+        return self.vertices[current_vertex.value].next_word()
 
     def generate_probability_mappings(self):
         for vertex in self.vertices.values():
